30551a63f37997b2e4298d72e30b72f5d57e1016
codejie <codejie@gmail.com>
2014-01-14 18:01:42 +0800

    PackGroup

--- IP-Game/src/jie/android/ip/screen/menu/MenuConfig.java
+++ IP-Game/src/jie/android/ip/screen/menu/MenuConfig.java
@@ -13,6 +13,11 @@ public interface MenuConfig {
 		}
 		
 		public interface Item {
+
+			public static final String Item_Bg = "cmd_bg";
+			public static final String FRAME = "box_frame";
+			public static final String TRAY = "tray";
+			public static final String BOX_0 = "box_a";
 			
 		}
 	}
@@ -35,6 +40,36 @@ public interface MenuConfig {
 		public interface Item {
 			public static final int NUM_TOTAL = 6;
 			public static final int NUM_PER_LINE = 3;
+
+			public static final int BASE_X = 96;
+			public static final int BASE_Y = 48;
+			
+			public static final int SPACE_X = 48;
+			public static final int SPACE_Y = 48;
+			
+			public static final int WIDTH = (ScreenConfig.WIDTH - SPACE_X * 2 - BASE_X * 2) / 3;
+			public static final int HEIGHT = 232;
+			
+			public static final int FRAME_HEIGHT = 2;
+			public static final int FRAME_WIDTH = WIDTH - 8;
+			public static final int FRAME_TOP_X = 4;
+			public static final int FRAME_TOP_Y = HEIGHT - 16;
+			public static final int FRAME_BOTTOM_X = 4;
+			public static final int FRAME_BOTTOM_Y = 16;
+
+			public static final int BLOCK_WIDTH = 24;
+			public static final int BLOCK_HEIGHT = 24;			
+			
+			public static final int COL_BASE = 8;
+			public static final int COL_SPACE = 4;
+			public static final int ROW_BASE = 8;
+			public static final int ROW_SPACE = 4;
+			
+			public static final int TRAY_WIDTH = 8;
+			public static final float TRAY_HEIGHT = 4;
+			
+			public static final int TRAY_BASE = 16;
+			public static final int TRAY_SPACE = 8;		
 		}		
 	}
 	
--- IP-Game/src/jie/android/ip/screen/menu/PackGroup.java
+++ IP-Game/src/jie/android/ip/screen/menu/PackGroup.java
@@ -1,5 +1,14 @@
 package jie.android.ip.screen.menu;
 
+import java.util.ArrayList;
+import java.util.Map.Entry;
+
+import aurelienribon.tweenengine.BaseTween;
+import aurelienribon.tweenengine.Timeline;
+import aurelienribon.tweenengine.Tween;
+import aurelienribon.tweenengine.TweenCallback;
+import aurelienribon.tweenengine.TweenManager;
+
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.BitmapFont.TextBounds;
@@ -10,11 +19,15 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import jie.android.ip.CommonConsts.ScreenConfig;
 import jie.android.ip.CommonConsts.ScreenPackConfig;
 import jie.android.ip.common.actor.BaseGroup;
+import jie.android.ip.common.actor.BaseGroupAccessor;
 import jie.android.ip.common.actor.ImageActor;
 import jie.android.ip.common.actor.LabelActor;
+import jie.android.ip.executor.Script;
 import jie.android.ip.screen.menu.MenuConfig.Const;
 import jie.android.ip.screen.menu.MenuConfig.Image;
 import jie.android.ip.screen.menu.MenuRenderer.PackGroupEventListener;
+import jie.android.ip.screen.play.Box;
+import jie.android.ip.utils.Extended.Pair;
 
 public class PackGroup extends BaseGroup {
 
@@ -48,21 +61,111 @@ public class PackGroup extends BaseGroup {
 			tb = i.getBounds();
 			i.setBounds((Const.Pack.WIDTH - tb.width) / 2, Const.Pack.HEIGHT * 0.33f, tb.width, tb.height);
 			this.addActor(i);
-			
 		}		
 	}
 	
+	//
+	private class ItemActor extends BaseGroup {
+
+		private final int id;
+		private final int status;
+		private final int score;
+		private final String script;
+		
+		public ItemActor(int id, int status, int score, final String script) {
+			this.id = id;
+			this.status = status;
+			this.score = score;
+			this.script = script;
+			
+			initStage();
+		}
+		
+		@Override
+		protected void initStage() {
+			ImageActor bg = new ImageActor(textureAtlas.findRegion(Image.Item.Item_Bg));
+			bg.setBounds(0, 0, Const.Item.WIDTH, Const.Item.HEIGHT);
+			this.addActor(bg);
+
+			final Script st = new Script(id); 
+			if (!st.loadString(script)) {
+				return;
+			}						
+			renderScript(st);
+		}
+		
+		private void renderScript(final Script st) {
+			ImageActor top = new ImageActor(textureAtlas.findRegion(Image.Item.FRAME));
+			top.setBounds(Const.Item.FRAME_TOP_X, Const.Item.FRAME_TOP_Y, Const.Item.FRAME_WIDTH, Const.Item.FRAME_HEIGHT);
+			this.addActor(top);
+			
+			ImageActor bottom = new ImageActor(textureAtlas.findRegion(Image.Item.FRAME));
+			bottom.setBounds(Const.Item.FRAME_BOTTOM_X, Const.Item.FRAME_BOTTOM_Y, Const.Item.FRAME_WIDTH, Const.Item.FRAME_HEIGHT);
+			this.addActor(bottom);
+			
+			loadBlock(st.getSource());
+			loadTray(st.getTray());
+		}
+		
+		private int colToBlockX(int col) {
+			return Const.Item.COL_BASE + (col - 1) * (Const.Item.BLOCK_WIDTH + Const.Item.COL_SPACE);
+		}
+		
+		private int rowToBlockY(int row) {
+			return Const.Item.ROW_BASE + (row) * (Const.Item.BLOCK_HEIGHT + Const.Item.ROW_SPACE);
+		}
+		
+		private int colToTrayX(int col) {
+			return Const.Item.TRAY_SPACE + (col - 1) * (Const.Item.TRAY_WIDTH);
+		}
+		
+		private int rowToTrayY() {
+			return Const.Item.TRAY_BASE;
+		}
+		
+		private void loadBlock(final ArrayList<Script.BlockData> blockData) {
+			for (final Script.BlockData block : blockData) {
+				ImageActor actor = makeActor(block.value, block.style);
+				actor.setBounds(colToBlockX(block.col), rowToBlockY(block.row), Const.Item.BLOCK_WIDTH, Const.Item.BLOCK_HEIGHT);
+				
+				this.addActor(actor);				
+			}
+		}
+		
+		private void loadTray(final Script.TrayData tray) {
+			ImageActor actor = new ImageActor(textureAtlas.findRegion(Image.Item.TRAY));// adapter.getResources().getSkin().getRegion("t"));
+			actor.setBounds(colToTrayX(tray.col), rowToTrayY(), Const.Item.TRAY_WIDTH, Const.Item.TRAY_HEIGHT);
+			
+			this.addActor(actor);			
+		}
+		
+		private final ImageActor makeActor(int value, int style) {
+			if (value == 0) {
+				return new ImageActor(textureAtlas.findRegion(Image.Item.BOX_0));// adapter.getResources().getSkin().getRegion("ic"));
+			} else {
+				return null;
+			}
+		}		
+	}
+	
+	//
 	private final MenuScreen screen;
 	private final TextureAtlas textureAtlas;
 	private final BitmapFont bitmapFont;
+	private final TweenManager tweenManager;
+	
 	private final PackGroupEventListener listener;
 	
 	private int itemStart = 0;
+	
+	private BaseGroup[] actorCache1 = new BaseGroup[Pack.NUM_PACK]; 
+	private BaseGroup[] actorCache2 = new BaseGroup[Pack.NUM_PACK];
 	
 	public PackGroup(final MenuScreen screen, final PackGroupEventListener listener) {
 		this.screen = screen;
 		this.textureAtlas = screen.getGame().getResources().getTextureAtlas(ScreenPackConfig.SCREEN_MENU);
 		this.bitmapFont = screen.getGame().getResources().getBitmapFont(24);
+		this.tweenManager = screen.getTweenManager();
 		this.listener = listener;
 
 		this.setBounds(0, 0, ScreenConfig.WIDTH, ScreenConfig.HEIGHT);		
@@ -74,7 +177,8 @@ public class PackGroup extends BaseGroup {
 	}
 	
 	public void loadPacks(final Pack[] packs) {
-		for (final Pack pack : packs) {
+		for (int i = 0; i < packs.length; ++ i) {
+			final Pack pack = packs[i];
 			PackActor actor = new PackActor(pack.getTitle(), pack.getTotal_all(), pack.getTotal_pass());
 			setPackBounds(pack.getId(), actor);
 			actor.addListener(new ClickListener() {
@@ -85,7 +189,9 @@ public class PackGroup extends BaseGroup {
 				}
 				
 			});
+			
 			this.addActor(actor);
+			actorCache1[i] = actor;
 		}
 		
 		itemStart = 0;
@@ -118,7 +224,7 @@ public class PackGroup extends BaseGroup {
 		int end = (items.length - start) <= MenuConfig.Const.Item.NUM_TOTAL ? items.length - start : MenuConfig.Const.Item.NUM_TOTAL;  
 		for (int i = 0; i < end; ++ i) {
 			final Pack.Item item = items[start + i];
-			ItemActor actor = new ItemActor(item.getStatus(), item.getScore(), item.getScript());
+			ItemActor actor = new ItemActor(item.getId(), item.getStatus(), item.getScore(), item.getScript());
 			setItemBounds(i, actor);
 			actor.addListener(new ClickListener() {
 				@Override
@@ -126,14 +232,83 @@ public class PackGroup extends BaseGroup {
 					listener.onPackItemClick(item.getId());
 				}				
 			});
+			
+			//actor.setVisible(false);
 			this.addActor(actor);
+			actorCache2[i] = actor;
 		}
 		
 		itemStart = start + end;
+
+		moveGroup();
 		
-		moveGroup(true);		
+		if (start == 0) {
+			movePackGroup(true);
+		} else {
+			moveItemGroup(true);
+		}
+	}
+
+	private void setItemBounds(int id, final ItemActor actor) {
+		int x = 0, y = 0;
+		switch(id) {
+		case 1:
+		case 2:
+		case 3:
+			x = Const.Item.BASE_X + (id - 1)* (Const.Item.WIDTH + Const.Item.SPACE_X);
+			y = - (Const.Item.BASE_Y + Const.Item.HEIGHT + Const.Item.SPACE_Y);
+			break;
+		case 4:
+		case 5:
+		case 6:
+			x = Const.Item.BASE_X + (id - 4)* (Const.Item.WIDTH + Const.Item.SPACE_X);
+			y = - Const.Item.BASE_Y;			
+			break;
+		default:
+			return;
+		}
+		
+		actor.setBounds(x, y, Const.Item.WIDTH, Const.Item.HEIGHT);		
+	}
+
+	private void moveGroup() {
+		final TweenCallback callback = new TweenCallback() {
+
+			@Override
+			public void onEvent(int type, BaseTween<?> source) {
+				for (final BaseGroup actor : actorCache1) {
+					PackGroup.this.removeActor(actor);
+				}
+				
+				actorCache1 = actorCache2;
+			}
+			
+		};
+		
+		// cache 1 out, cache 2 in
+		Timeline timeline = Timeline.createParallel();
+		for (int i = 0; i < actorCache1.length; ++ i) {
+			final BaseGroup actor = actorCache1[i];
+			timeline.push(Tween.to(actor, BaseGroupAccessor.POSITION_Y, 0.2f + 0.1f * i).targetRelative(ScreenConfig.HEIGHT));
+		}
+		timeline.delay(0.2f);
+		for (int i = 0; i < actorCache2.length; ++ i) {
+			final BaseGroup actor = actorCache2[i];
+			if (actor != null) {
+//				timeline.push(Tween.set(actor, BaseGroupAccessor.POSITION_Y).targetRelative(-Const.Item.HEIGHT));
+				timeline.push(Tween.to(actor, BaseGroupAccessor.POSITION_Y, 0.2f + 0.1f * i).targetRelative(Const.Item.BASE_Y + Const.Item.HEIGHT + Const.Item.SPACE_Y));
+			}
+		}
+		
+		timeline.setCallback(callback);		
+		timeline.start(tweenManager);
 	}
 	
-	
+	private void movePackGroup(boolean out) {
+		
+	}
 
+	private void moveItemGroup(boolean out) {
+		
+	}
 }
